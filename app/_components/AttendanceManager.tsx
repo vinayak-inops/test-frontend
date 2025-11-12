acc51527cdf8a0335ed156aac1e9eb8d36cab220
vinayak-inops <vinayak@inops.tech>
2025-11-12 22:24:56 +0530

    test 2 docker update

--- app/_components/AttendanceManager.tsx
+++ app/_components/AttendanceManager.tsx
@@ -12,7 +12,7 @@ export default function TestPage() {
   return (
     <div className="p-6">
       <button onClick={testApi} className="bg-blue-500 text-white p-2 rounded">
-        Application tester testing Docuker Hub
+        Application tester testing Docuker Hub TEST 1
       </button>
     </div>
   );
