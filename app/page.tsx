08d3bcd833d2d6576e54b9cbd0966470e2cabd5a
vinayak-inops <vinayak@inops.tech>
2025-11-12 23:28:23 +0530

    test 3 docker update .env

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import TestPage from "./_components/AttendanceManager";
 export default function Home() {
  
   return <div>
-    <p>{process.env.NEXT_PUBLIC_APP_NAME} this env value test 1</p>
+    <p>{process.env.NEXT_PUBLIC_APP_NAME} this env value test 2</p>
     <p>{process.env.NEXT_PUBLIC_ENV} this env value test 1</p>
     <TestPage />
   </div>;
