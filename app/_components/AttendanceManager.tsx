dd38f300a1ea23c2c5aac0a9d44b388ca2255a3e
VinayakSoragavi <soragavivinayak@gmail.com>
2025-11-12 19:51:35 +0530

    git action test

---
+++ app/_components/AttendanceManager.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import apiHelper from "vinayak-api-helper";
+
+export default function TestPage() {
+  async function testApi() {
+    try {
+      
+      // call GET API instead of POST
+      const res = await apiHelper.organization.countryGet.getAllCountries();
+
+      console.log("API Response:", res);
+    } catch (error: any) {
+    }
+  }
+
+  return (
+    <div className="p-6">
+      <button onClick={testApi} className="bg-blue-500 text-white p-2 rounded">
+        Call GET Attendance API
+      </button>
+    </div>
+  );
+}
