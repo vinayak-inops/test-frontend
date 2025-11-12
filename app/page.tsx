25993e578eae5a181573bf5ee77fb25edea624c1
vinayak-inops <vinayak@inops.tech>
2025-11-12 22:41:46 +0530

    test 3 docker update .env

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,9 @@ import TestPage from "./_components/AttendanceManager";
 
 export default function Home() {
  
-  return <TestPage />;
+  return <div>
+
+    {process.env.NEXT_PUBLIC_ENV} {process.env.NEXT_PUBLIC_APP_NAME}
+    <TestPage />
+  </div>;
 }
